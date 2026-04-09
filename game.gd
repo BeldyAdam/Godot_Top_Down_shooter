825effdffd9c7d3881577f81ceafc18321c9153a
Béldy Ádám <beldy.adam@gmail.com>
2026-04-09 20:44:50 +0200

    Score system ready -> Waiting for UI elements; GameOver Screen ready UI elements and graphics added

--- game.gd
+++ game.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 var score := 0
+@onready var score_label = %ScoreLabel
+
+func _ready():
+	score_label.text = "Score: 0"
 
 var enemy_scenes = [
 	preload("res://mob.tscn"),
@@ -22,6 +26,12 @@ func _on_player_health_depleted():
 	%GameOver.visible = true
 	get_tree().paused = true
 	
+	score = 0
+	%ScoreLabel.text = "Score: 0"
+	
 func add_score(amount):
 	score += amount
-	print("Score: ", score)
+	score_label.text = "Score: " + str(score)
+
+func _on_back_to_main_menu_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
