1fc6256230615b77affc51b1e7d7a9a0be006bc1
Béldy Ádám <beldy.adam@gmail.com>
2026-04-09 22:48:25 +0200

    Game restart bug fixed

--- game.gd
+++ game.gd
@@ -34,4 +34,5 @@ func add_score(amount):
 	score_label.text = "Score: " + str(score)
 
 func _on_back_to_main_menu_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://main_menu.tscn")
